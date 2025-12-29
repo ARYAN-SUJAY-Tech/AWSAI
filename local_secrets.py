3bf2dd0d70fd4b39a1aa8ffe6f93c0b72b210521
ARYAN SUJAY <aryan.s0507@gmail.com>
2025-12-29 16:16:16 +0530

    Added API key in local_secrets.py

--- local_secrets.py
+++ local_secrets.py
@@ -1,2 +1,3 @@
 #secrets.py
-OPENAI_API_KEY = "sk-proj-..."
+OPENAI_API_KEY = "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"
+
